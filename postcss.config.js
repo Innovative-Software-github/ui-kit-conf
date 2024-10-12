adb16a7b64fa78670e870c606ef71db4ffd7b6fc
Владимир Крынин <krinin.vlad2804@icloud.com>
2024-10-13 02:52:41 +0300

    add rollup postcss, update ts config

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = {
+  plugins: [
+    postcssPresetEnv({
+      preserve: false,
+      stage: 3,
+      features: {
+        'nesting-rules': true,
+        'color-mod-function': {
+          unresolved: 'warn',
+        },
+      },
+      autoprefixer: {
+        grid: true,
+      },
+    }),
+  ],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-export { Icon, IIconProps } from './components/Icon/Icon';
+export { Icon } from './components/Icon/Icon';
+export type { IIconProps } from './components/Icon/Icon';
 export { IconType } from './components/Icon/IconsMapping';
