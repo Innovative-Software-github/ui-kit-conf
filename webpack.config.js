917bdf55444c27f7f55857508a3e75d68e1d09a6
Владимир Крынин <krinin.vlad2804@icloud.com>
2024-10-13 00:06:49 +0300

    new config

--- webpack.config.js
+++ webpack.config.js
@@ -2,63 +2,56 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.ts',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'umd',
-    library: 'ui-kit-conf',
-    umdNamedDefine: true,
-    clean: true,
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  externals: {
-    react: 'react',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.module\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              esModule: true,
+    mode: 'production',
+    entry: './src/index.ts',
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: "umd",
+        clean: true
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx']
+    },
+    externals: {
+        react: 'react'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.module\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[name]__[local]__[hash:base64:5]',
+                            },
+                            importLoaders: 1,
+                            esModule: true,
+                        }
+                    }
+                ],
             },
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: `conf__[name]__[local]__[hash:base64:5]`,
-              },
-              esModule: true,
+            {
+                test: /\.css$/,
+                exclude: /\.module\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ],
             },
-          },
+            {
+                test: /\.(ts|tsx)?$/,
+                use: ['ts-loader'],
+                exclude: /node_modules/
+            }
         ],
-      },
-      {
-        test: /\.css$/,
-        exclude: /\.module\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-        ],
-      },
-      {
-        test: /\.(ts|tsx)?$/,
-        use: ['ts-loader'],
-        exclude: /node_modules/,
-      },
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
     ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'styles.css',
-    }),
-  ],
-};
+}
