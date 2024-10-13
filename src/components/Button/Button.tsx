51f7c5b454572065881fb3410a4045ab4dd3f439
T1mka <120297318+T1ma1313@users.noreply.github.com>
2024-10-13 21:10:45 +0500

    DS-91 add button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,42 @@
 import React from 'react';
+import { clsx } from 'clsx';
 import cls from './Button.module.css';
 
-export const Button = ({
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: string;
+  className?: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  buttonType?: 'filed' | 'outlined' | 'text';
+  icon?: string;
+  isLoading?: boolean;
+  disabled?: boolean;
+}
+
+export const Button: React.FC<ButtonProps> = ({
   children,
   className,
   onClick,
-  type,
+  buttonType,
   icon,
+  isLoading,
+  disabled,
   ...props
 }) => (
   <button
-    className={cls.button}
-    type={type}
+    type="button"
+    className={clsx(
+      cls.button,
+      { [cls.filed]: buttonType === 'filed' },
+      { [cls.outlined]: buttonType === 'outlined' },
+      { [cls.text]: buttonType === 'text' },
+    )}
     onClick={onClick}
+    disabled={disabled || isLoading}
     {...props}
   >
-    {icon}
+    <div>
+      {icon}
+    </div>
     {children}
   </button>
 );
