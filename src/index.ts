0d04e20b2a98558777641aeb056fefa4d6e866ce
Владимир Крынин <krinin.vlad2804@icloud.com>
2024-10-19 00:04:24 +0300

    DS-106, add spinner

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import './theme/variables.css';
 
 export { Icon } from './components/Icon/Icon';
 export { IconType } from './components/Icon/IconsMapping';
+export { Spinner } from './components/Spinner/Spinner';
