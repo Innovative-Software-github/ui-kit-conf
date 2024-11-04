4daed34237217d32df4bdd2e9f5f3433857af9ab
Владимир Крынин <krinin.vlad2804@icloud.com>
2024-11-04 15:19:05 +0500

    DS-93, add checkbox component

--- src/components/Checkbox/Checkbox.module.css.d.ts
+++ src/components/Checkbox/Checkbox.module.css.d.ts
@@ -1,12 +1,9 @@
 declare const cls: {
-    readonly checkboxContainer: string;
-    readonly checkboxInput: string;
-    readonly checkboxIcon: string;
-    readonly checkboxLabel: string;
-    readonly disabled: string;
-    readonly active: string;
-    readonly checked: string;
-    readonly unchecked: string;
+  readonly container: string;
+  readonly checkbox: string;
+  readonly label: string;
+  readonly disabled: string;
+  readonly checked: string;
 };
 
 export = cls;
--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,56 +1,38 @@
 import React from 'react';
 import clsx from 'clsx';
+
 import { Icon } from '../Icon/Icon';
 import { IconType } from '../Icon/IconsMapping';
-import clx from './Checkbox.module.css';
 
-interface ICheckboxProps {
-    label: string;
-    checked?: boolean;
-    disabled?: boolean;
-    onChange?: (checked: boolean) => void;
+import cls from './Checkbox.module.css';
+
+export interface ICheckboxProps {
+  label?: string;
+  checked: boolean;
+  disabled?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const Checkbox: React.FC<ICheckboxProps> = ({
   label,
-  checked = false,
+  checked,
   disabled = false,
   onChange,
-}) => {
-  const [isChecked, setIsChecked] = React.useState(checked);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!disabled) {
-      const newChecked = e.currentTarget.checked;
-
-      setIsChecked(newChecked);
-      if (onChange) {
-        onChange(newChecked);
-      }
-    }
-  };
-
-  return (
-    <label className={clsx(clx.checkboxContainer, {
-      [clx.disabled]: disabled,
-      [clx.active]: !disabled,
-    })}
-    >
-      <input
-        type="checkbox"
-        checked={isChecked}
-        disabled={disabled}
-        onChange={handleChange}
-        className={clx.checkboxInput}
-      />
-      <span className={clsx(clx.checkboxIcon, {
-        [clx.checked]: isChecked,
-        [clx.unchecked]: !isChecked,
-      })}
-      >
-        {isChecked ? <Icon type={IconType.Checkmark_20} height={15} width={15} /> : ''}
-      </span>
-      <span className={clx.checkboxLabel}>{label}</span>
-    </label>
-  );
-};
+}) => (
+  <label className={clsx(cls.container, {
+    [cls.checked]: checked,
+    [cls.disabled]: disabled,
+  })}
+  >
+    <input
+      type="checkbox"
+      checked={checked}
+      disabled={disabled}
+      onChange={onChange}
+    />
+    <span className={cls.checkbox}>
+      {checked && <Icon type={IconType.Checkmark_20} height={18} width={18} /> }
+    </span>
+    {label && <span className={cls.label}>{label}</span>}
+  </label>
+);
