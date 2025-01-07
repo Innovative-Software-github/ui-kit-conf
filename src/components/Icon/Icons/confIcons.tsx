870187347a6d106f73699a204df5b2e0afcd6072
Ирина <irina@MacBook-Air-Irina.local>
2025-01-07 14:27:04 +0700

    DS-136, add new icons

--- src/components/Icon/Icons/confIcons.tsx
+++ src/components/Icon/Icons/confIcons.tsx
@@ -2,116 +2,135 @@
 import * as React from 'react';
 
 export const ArrowRight_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="m6.687 18.334-1.479-1.48L12.062 10 5.208 3.146l1.48-1.479L15.02 10z" fill="currentColor" />
   </svg>
 );
 
 export const ArrowLeft_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M13.333 18.333 5 10l8.333-8.333 1.48 1.479L7.958 10l6.855 6.854z" fill="currentColor" />
   </svg>
 );
 
 export const ArrowDown_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="m1.782 6.573 1.479-1.48 6.854 6.855 6.854-6.854 1.48 1.479-8.334 8.333z" fill="currentColor" />
   </svg>
 );
 
 export const ArrowTop_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="m18.448 13.427-1.48 1.48-6.853-6.855-6.855 6.854-1.479-1.479 8.334-8.333z" fill="currentColor" />
   </svg>
 );
 
 export const Close_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M5.567 15.763 4.4 14.597 9.067 9.93 4.4 5.263l1.167-1.166 4.667 4.666L14.9 4.097l1.167 1.166L11.4 9.93l4.667 4.667-1.167 1.166-4.666-4.666z" fill="currentColor" />
   </svg>
 );
 
 export const Selected_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M10 12.5 5.833 8.333h8.333z" fill="currentColor" />
   </svg>
 );
 
 export const Checkmark_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M16.666 5 7.5 14.167 3.333 10" stroke="currentColor" strokeWidth="1.667" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
 
 export const Profile_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M16.666 17.5v-1.667a3.333 3.333 0 0 0-3.333-3.333H6.666a3.333 3.333 0 0 0-3.333 3.333V17.5m10-11.667a3.333 3.333 0 1 1-6.667 0 3.333 3.333 0 0 1 6.667 0" stroke="currentColor" strokeWidth="1.667" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
 
 export const Trash_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M6.138 17.388q-.688 0-1.177-.49a1.6 1.6 0 0 1-.49-1.177V4.888h-.833V3.22h4.166v-.833h5v.833h4.167v1.667h-.833V15.72q0 .687-.49 1.177t-1.177.49zm8.333-12.5H6.138V15.72h8.333zm-6.667 9.166h1.667v-7.5H7.804zm3.334 0h1.666v-7.5h-1.666z" fill="currentColor" />
   </svg>
 );
 
 export const Edit_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M4.167 15.833h1.187L13.5 7.688 12.313 6.5l-8.146 8.146zM2.5 17.5v-3.542l11-10.979q.25-.229.552-.354t.636-.125.645.125q.314.125.542.375l1.146 1.167q.25.229.364.541a1.8 1.8 0 0 1 0 1.26 1.6 1.6 0 0 1-.364.553L6.04 17.5zM12.896 7.104l-.584-.604L13.5 7.688z" fill="currentColor" />
   </svg>
 );
 
 export const Link_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M13.96 10.786v3.334q0 1.728-1.22 2.947-1.218 1.22-2.947 1.22t-2.948-1.22q-1.22-1.218-1.219-2.947v-3.334h1.667v3.334q0 1.041.729 1.77t1.77.73 1.771-.73a2.4 2.4 0 0 0 .73-1.77v-3.334zm-3.334 2.5H8.959V6.62h1.667zm3.333-4.167h-1.666V5.786a2.4 2.4 0 0 0-.73-1.77 2.4 2.4 0 0 0-1.77-.73 2.4 2.4 0 0 0-1.771.73 2.4 2.4 0 0 0-.73 1.77V9.12H5.627V5.786q0-1.729 1.219-2.948Q8.063 1.62 9.793 1.62q1.728 0 2.948 1.22 1.218 1.218 1.218 2.947z" fill="currentColor" />
   </svg>
 );
 
 export const Eye_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M10.181 12.911q1.563 0 2.657-1.094 1.093-1.093 1.093-2.656t-1.093-2.656Q11.744 5.41 10.18 5.41T7.525 6.505 6.431 9.16t1.094 2.656q1.095 1.095 2.656 1.094m0-1.5a2.17 2.17 0 0 1-1.593-.656A2.17 2.17 0 0 1 7.93 9.16q0-.937.657-1.594a2.17 2.17 0 0 1 1.593-.656 2.17 2.17 0 0 1 1.594.656q.657.658.656 1.594a2.17 2.17 0 0 1-.656 1.594 2.17 2.17 0 0 1-1.594.656m0 4q-3.041 0-5.541-1.698-2.5-1.699-3.625-4.552Q2.14 6.307 4.64 4.61t5.541-1.698 5.542 1.698 3.625 4.552q-1.125 2.854-3.625 4.552t-5.542 1.698m0-1.666q2.354 0 4.323-1.24a8.15 8.15 0 0 0 3.01-3.344 8.15 8.15 0 0 0-3.01-3.344 7.95 7.95 0 0 0-4.323-1.24q-2.354 0-4.323 1.24a8.14 8.14 0 0 0-3.01 3.344 8.14 8.14 0 0 0 3.01 3.344 7.95 7.95 0 0 0 4.323 1.24" fill="currentColor" />
   </svg>
 );
 
 export const EyeOff_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M13.598 10.661 12.39 9.453q.187-.98-.563-1.834T9.89 6.953L8.68 5.744q.354-.166.72-.25a3.5 3.5 0 0 1 .78-.083q1.563 0 2.657 1.094Q13.93 7.598 13.93 9.16q0 .417-.083.781a3.5 3.5 0 0 1-.25.72m2.667 2.625-1.209-1.166a9 9 0 0 0 1.407-1.323q.614-.72 1.052-1.636a8.2 8.2 0 0 0-2.99-3.344q-1.949-1.24-4.344-1.24-.603 0-1.187.084t-1.146.25L6.556 3.62a9.2 9.2 0 0 1 3.625-.708q3.147 0 5.605 1.74 2.457 1.74 3.562 4.51a9.8 9.8 0 0 1-1.26 2.281 9.2 9.2 0 0 1-1.823 1.844m.416 5.125-3.5-3.458a10 10 0 0 1-1.468.344q-.74.114-1.532.114-3.145 0-5.604-1.74t-3.562-4.51q.437-1.104 1.104-2.052a9.6 9.6 0 0 1 1.52-1.698l-2.29-2.334L2.515 1.91l15.333 15.333zM4.806 6.578q-.603.541-1.104 1.187-.5.646-.854 1.396a8.2 8.2 0 0 0 2.99 3.344q1.947 1.24 4.343 1.24.417 0 .813-.053.396-.051.812-.114l-.75-.792a4 4 0 0 1-.437.094q-.21.03-.438.031-1.562 0-2.656-1.094-1.094-1.093-1.094-2.656 0-.229.032-.437a4 4 0 0 1 .093-.438z" fill="currentColor" />
   </svg>
 );
 
 export const Cancel_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="m6.65 14.665 3-3 3 3 1.167-1.167-3-3 3-3-1.166-1.166-3 3-3-3-1.167 1.166 3 3-3 3zm3 4.167a8.1 8.1 0 0 1-3.25-.657 8.4 8.4 0 0 1-2.645-1.78 8.4 8.4 0 0 1-1.781-2.647 8.1 8.1 0 0 1-.657-3.25q0-1.729.657-3.25a8.4 8.4 0 0 1 1.78-2.645A8.4 8.4 0 0 1 6.402 2.82a8.1 8.1 0 0 1 3.25-.656q1.729 0 3.25.656a8.4 8.4 0 0 1 2.646 1.782 8.4 8.4 0 0 1 1.78 2.645 8.1 8.1 0 0 1 .657 3.25 8.1 8.1 0 0 1-.656 3.25 8.4 8.4 0 0 1-1.781 2.646 8.4 8.4 0 0 1-2.646 1.782 8.1 8.1 0 0 1-3.25.656m0-1.667q2.793 0 4.73-1.937t1.937-4.73q0-2.79-1.937-4.729-1.937-1.937-4.73-1.937-2.79 0-4.728 1.937T2.984 10.5q0 2.79 1.938 4.728t4.729 1.938" fill="currentColor" />
   </svg>
 );
 
 export const Done_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="m8.834 13.834 5.875-5.875-1.167-1.167L8.834 11.5 6.459 9.125l-1.167 1.167zm1.166 4.5a8.1 8.1 0 0 1-3.25-.657 8.4 8.4 0 0 1-2.646-1.78 8.4 8.4 0 0 1-1.78-2.647A8.1 8.1 0 0 1 1.666 10q0-1.729.656-3.25a8.4 8.4 0 0 1 1.781-2.646 8.4 8.4 0 0 1 2.646-1.78A8.1 8.1 0 0 1 10 1.666q1.73 0 3.25.656a8.4 8.4 0 0 1 2.646 1.781 8.4 8.4 0 0 1 1.781 2.646 8.1 8.1 0 0 1 .657 3.25 8.1 8.1 0 0 1-.657 3.25 8.4 8.4 0 0 1-1.78 2.646 8.4 8.4 0 0 1-2.647 1.781 8.1 8.1 0 0 1-3.25.657m0-1.667q2.792 0 4.73-1.938 1.937-1.937 1.937-4.729 0-2.79-1.938-4.729Q12.793 3.334 10 3.334q-2.79 0-4.729 1.937-1.937 1.938-1.937 4.73 0 2.79 1.937 4.729 1.938 1.937 4.73 1.937" fill="currentColor" />
   </svg>
 );
 
 export const Location_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M10 10q.687 0 1.177-.49.49-.489.49-1.176 0-.688-.49-1.177A1.6 1.6 0 0 0 10 6.667q-.688 0-1.177.49-.49.489-.49 1.177 0 .687.49 1.177Q9.312 10 10 10m0 6.125q2.541-2.332 3.77-4.24Q15 9.98 15 8.5q0-2.27-1.448-3.718Q12.103 3.333 10 3.334q-2.104 0-3.552 1.448Q4.999 6.229 5 8.5q0 1.48 1.229 3.386t3.77 4.24m0 2.209q-3.355-2.854-5.01-5.302Q3.332 10.584 3.332 8.5q0-3.125 2.01-4.979T10 1.667t4.656 1.854 2.01 4.98q0 2.083-1.656 4.53-1.656 2.449-5.01 5.303" fill="currentColor" />
   </svg>
 );
 
 export const Plus_20 = (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="M9.633 11.183h-5V9.517h5v-5H11.3v5h5v1.666h-5v5H9.633z" fill="currentColor" />
   </svg>
 );
 
 export const Search_20 = (
-  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" color="inherit" xmlns="http://www.w3.org/2000/svg">
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
     <path d="m14.333 15.5-5.25-5.25a5.08 5.08 0 0 1-3.167 1.083q-2.27 0-3.843-1.573T.5 5.917t1.573-3.844T5.917.5 9.76 2.073t1.573 3.844a5.08 5.08 0 0 1-1.083 3.166l5.25 5.25zM5.917 9.667q1.562 0 2.656-1.094t1.094-2.656T8.573 3.26Q7.478 2.167 5.917 2.167T3.26 3.26Q2.167 4.355 2.167 5.917T3.26 8.573t2.657 1.094" fill="currentColor" />
   </svg>
 );
 
 export const Logotype = (
-  <svg width="135" height="16" viewBox="0 0 135 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="135" height="16" viewBox="0 0 135 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path d="M0 15.5V.5h9v3H3v3h6v3H3v3h6v3zm21 0v-3h-3v-6h-3v-6h3v6h3v6h3v3zm6-9v-6h3v6zm-3 6v-6h3v6zm12 3V.5h9v3h-6v3h6v3h-6v3h6v3zm15 0V.5h3v3h3v3h3v3h3v-9h3v15h-3v-3h-3v-3h-3v-3h-3v9zm24 0v-12h-3v-3h9v3h-3v12z" fill="#454545" />
     <path d="M87 15.5V.5h3v6h6v-6h3v15h-3v-6h-6v6zm21 0v-3h-3V.5h3v12h6v3zm6-3V.5h3v12zm9 3V.5h9v3h3v9h-3v3zm3-9h5.88v-3H126zm0 6h5.88v-3H126z" fill="#11C5BA" />
+  </svg>
+);
+
+export const Logotype_white = (
+  <svg width="180" height="20" viewBox="0 0 180 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 20V0h12v4H4v4h8v4H4v4h8v4zm28 0v-4h-4V8h-4V0h4v8h4v8h4v4zm8-12V0h4v8zm-4 8V8h4v8zm16 4V0h12v4h-8v4h8v4h-8v4h8v4zm20 0V0h4v4h4v4h4v4h4V0h4v20h-4v-4h-4v-4h-4V8h-4v12zm32 0V4h-4V0h12v4h-4v16z" fill="#fff" />
+    <path d="M116 20V0h4v8h8V0h4v20h-4v-8h-8v8zm28 0v-4h-4V0h4v16h8v4zm8-4V0h4v16zm12 4V0h12v4h4v12h-4v4zm4-12h7.84V4H168zm0 8h7.84v-4H168z" fill="#11C5BA" />
+  </svg>
+);
+
+export const TGLink_44 = (
+  <svg width="44" height="44" viewBox="0 0 44 44" fill="currentColor" color="inherit" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" clipRule="evenodd" d="M22 44c12.15 0 22-9.85 22-22S34.15 0 22 0 0 9.85 0 22s9.85 22 22 22m9.67-29.945c.17-1.927-1.866-1.133-1.866-1.133-1.502.622-3.052 1.254-4.618 1.893-4.858 1.981-9.876 4.028-14.15 6.043-2.318.85-.961 1.7-.961 1.7l3.674 1.134c1.696.51 2.6-.057 2.6-.057l7.915-5.385c2.827-1.927 2.148-.34 1.47.34l-5.936 5.669c-.904.793-.452 1.474-.056 1.814 1.121.99 3.88 2.8 5.085 3.59.313.206.521.342.568.378.282.226 1.809 1.247 2.827 1.02 1.017-.227 1.13-1.53 1.13-1.53l1.357-8.9c.203-1.545.437-3.03.623-4.216.176-1.12.31-1.974.338-2.36" />
+  </svg>
+);
+
+export const VKLink_44 = (
+  <svg width="44" height="44" viewBox="0 0 44 44" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" clipRule="evenodd" d="M22 44c12.15 0 22-9.85 22-22S34.15 0 22 0 0 9.85 0 22s9.85 22 22 22m10.742-16.464c-.023-.023-.046-.046-.046-.092-.365-.73-1.187-1.644-2.283-2.648a7.4 7.4 0 0 1-.832-.817 5 5 0 0 0-.173-.188c-.274-.365-.274-.639-.182-1.004.084-.254.404-.665.959-1.377l.137-.176c.114-.143.21-.269.298-.381a6 6 0 0 1 .523-.623c1.37-1.827 2.01-3.014 1.827-3.562l-.091-.091c-.092-.092-.183-.183-.366-.183-.182-.091-.456-.091-.73 0h-4.11l-.091.091c-.091.092-.091.092-.091.183-.292.802-.642 1.488-1.004 2.196l-.275.544c-.274.456-.548.913-.822 1.278l-.202.274c-.17.232-.304.414-.437.548-.065.065-.13.118-.19.168-.11.091-.208.17-.267.289-.091.091-.274.182-.274.091-.091 0-.183 0-.183-.091l-.273-.274c-.052-.051-.074-.132-.1-.224a1.3 1.3 0 0 0-.083-.233v-4.566a.7.7 0 0 0-.046-.183.7.7 0 0 1-.046-.182l-.274-.274a.5.5 0 0 0-.182-.092.5.5 0 0 1-.183-.091 8.2 8.2 0 0 0-1.644-.183c-1.46 0-2.465.092-2.83.274-.183.092-.366.183-.457.366-.183.182-.183.274-.092.274.457.09.822.273 1.005.547l.091.183c.057.057.078.148.108.274.018.078.04.17.075.274.091.274.091.548.091.913v1.462c0 .257-.029.456-.054.63a3 3 0 0 0-.037.374c0 .274-.091.457-.183.548q-.038.081-.061.137c-.038.091-.057.137-.121.137 0 0 0 .091-.092.091-.038 0-.075.016-.12.034a.6.6 0 0 1-.245.058c-.091 0-.274-.092-.365-.183a1.1 1.1 0 0 1-.457-.457q-.274-.273-.548-.822a7 7 0 0 0-.195-.352 4.8 4.8 0 0 1-.444-.926l-.183-.365c-.091-.183-.274-.548-.456-1.005-.092-.228-.183-.434-.274-.64-.092-.205-.183-.41-.274-.638a1.3 1.3 0 0 0-.274-.366h-.092s-.091-.091-.182-.091a.3.3 0 0 1-.137-.046.3.3 0 0 0-.137-.045h-3.288c-.365 0-.548.09-.64.182l-.09.092v.182a.7.7 0 0 0 .09.366c.457 1.095 1.005 2.191 1.553 3.287.382.7.764 1.357 1.084 1.906q.211.36.377.651c.172.24.33.48.486.716.259.391.508.769.793 1.11q.685.823.822 1.097l.365.365.274.274c.183.183.457.457.822.73.365.275.822.549 1.279.823s1.004.456 1.552.639c.64.183 1.187.274 1.735.183h1.37a.83.83 0 0 0 .64-.274l.09-.092c0-.046.024-.068.047-.091.022-.023.045-.046.045-.091v-.366c0-.365 0-.639.091-.913.04-.12.063-.223.083-.315a1.3 1.3 0 0 1 .1-.324c.091-.183.183-.274.274-.366l.183-.182h.091c.183-.092.457 0 .64.182.273.183.547.457.73.731s.456.548.822.913c.365.366.64.64.822.73l.274.183c.183.092.365.183.64.274.273.092.456.092.638.092l3.105-.091c.274 0 .548-.092.731-.183s.274-.183.274-.365v-.366c-.183-.182-.183-.274-.183-.365 0-.046-.023-.069-.045-.091" fill="currentColor" />
   </svg>
 );
